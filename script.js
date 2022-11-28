6087e029066f14ac7f67a10af750dd30f1a33afc
rodrigopenayo98 <91923422+rodrigopenayo98@users.noreply.github.com>
2022-11-27 21:55:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -42,3 +42,25 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 }
+
+// funcionalidad del formulario
+
+const $form = document.querySelector("#form");
+
+$form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event){
+ event.preventDefault();
+ const form = new FormData(this);
+ const response = await fetch(this.action,{
+    method: this.method,
+    body: form,
+    headers:{
+        "Accept": "application/json"
+    }
+ })
+ if(response.ok){
+    alert("Gracias por contactarme!");
+    this.reset()
+ }
+}
